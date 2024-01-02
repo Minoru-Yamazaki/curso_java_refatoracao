dd63d31855b90eec2d0ad9f4b0261ab7c39eea5c
Minoru-Yamazaki <minoru.rhcp@gmail.com>
2024-01-02 15:58:23 -0300

    Initial commit

---
+++ src/main/java/br/com/alura/services/util/Requisicao.java
@@ -0,0 +1,29 @@
+package br.com.alura.services.util;
+
+import com.google.gson.JsonObject;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Requisicao {
+
+    public static HttpResponse<String> post(String uri, JsonObject json) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(uri))
+                .header("Content-Type", "application/json")
+                .method("POST", HttpRequest.BodyPublishers.ofString(json.toString()))
+                .build();
+        return HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    public static HttpResponse<String> get(String uri) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(uri))
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        return HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+    }
+}
