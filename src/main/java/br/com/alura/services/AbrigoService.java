8367f437252f81e8b3832fbf1a43ce9b08b3fa80
Minoru-Yamazaki <minoru.rhcp@gmail.com>
2024-01-04 20:04:38 -0300

    feat: Adicionado injeção da dependência da requisição

--- src/main/java/br/com/alura/services/AbrigoService.java
+++ src/main/java/br/com/alura/services/AbrigoService.java
@@ -12,18 +12,36 @@ import java.util.List;
 
 public class AbrigoService {
 
+    private final Requisicao requisicao;
+
+    public AbrigoService(Requisicao requisicao) {
+        this.requisicao = requisicao;
+    }
+
     public void cadastrarAbrigo() throws IOException, InterruptedException {
         Abrigo abrigo = formularioCadastroAbrigo();
 
-        HttpResponse<String> response = Requisicao.post("http://localhost:8080/abrigos", abrigo);
+        HttpResponse<String> response = requisicao.post("http://localhost:8080/abrigos", abrigo);
         int statusCode = response.statusCode();
-        String responseBody = response.body();
+
         if (statusCode == 200) {
             System.out.println("Abrigo cadastrado com sucesso!");
-            System.out.println(responseBody);
+            System.out.println(response.body());
         } else if (statusCode == 400 || statusCode == 500) {
             System.out.println("Erro ao cadastrar o abrigo:");
-            System.out.println(responseBody);
+            System.out.println(response.body());
+        }
+    }
+
+    public void listarAbrigo() throws IOException, InterruptedException {
+        HttpResponse<String> response = requisicao.get("http://localhost:8080/abrigos");
+        List<Abrigo> abrigos = Arrays.stream(new ObjectMapper().readValue(response.body(), Abrigo[].class)).toList();
+
+        if (abrigos.isEmpty()) {
+            System.out.println("Não há abrigos cadastrados");
+        } else {
+            System.out.println("Abrigos cadastrados:");
+            abrigos.forEach(Abrigo::imprimirAbrigoCadastrado);
         }
     }
 
@@ -35,15 +53,5 @@ public class AbrigoService {
         abrigo.setEmail(Formulario.getString("Digite o email do abrigo:"));
 
         return abrigo;
-    }
-
-    public void listarAbrigo() throws IOException, InterruptedException {
-        HttpResponse<String> response = Requisicao.get("http://localhost:8080/abrigos");
-        List<Abrigo> abrigos = Arrays.stream(new ObjectMapper().readValue(response.body(), Abrigo[].class)).toList();
-
-        System.out.println("Abrigos cadastrados:");
-        for (Abrigo abrigo : abrigos) {
-            System.out.println(abrigo.getId() + " - " + abrigo.getNome());
-        }
     }
 }
