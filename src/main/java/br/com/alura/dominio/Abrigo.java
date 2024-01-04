2d6b72c03a777650aa4c29c16857234893bfe815
Minoru-Yamazaki <minoru.rhcp@gmail.com>
2024-01-03 22:09:40 -0300

    feat: Adicionado classes de domínio

---
+++ src/main/java/br/com/alura/dominio/Abrigo.java
@@ -0,0 +1,43 @@
+package br.com.alura.dominio;
+
+import java.util.List;
+
+public class Abrigo extends Domain{
+
+    private String nome;
+    private String telefone;
+    private String email;
+    private List<Pet> pets;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public List<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(List<Pet> pets) {
+        this.pets = pets;
+    }
+}
---
+++ src/main/java/br/com/alura/dominio/Domain.java
@@ -0,0 +1,10 @@
+package br.com.alura.dominio;
+
+public abstract class Domain {
+
+    Long id;
+
+    public Long getId() {
+        return id;
+    }
+}
